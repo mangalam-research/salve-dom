2ce83ad8bf2ce4019d9ece72f6839630b3ebeff0
Louis-Dominique Dubeau <ldd@lddubeau.com>
2019-05-21 10:13:00 -0400

    build: cleanup test file conversion

--- tasks/convert-test-files.js
+++ tasks/convert-test-files.js
@@ -1,31 +1,27 @@
 "use strict";
 
 const path = require("path");
-const glob = require("glob");
+const util = require("util");
+
+const glob = util.promisify(require("glob"));
 
 const { cprp, exec, existsInFile, mkdirpAsync } = require("./util");
 
-glob("test/data/**", { nodir: true }, (err, files) => {
+async function convert() {
+  const files = await glob("test/data/**", { nodir: true });
+
   const xsl = "test/xml-to-xml-tei.xsl";
-  const promises = [];
-  for (const file of files) {
-    const p = (async () => {
-      const ext = path.extname(file);
-      const destName = path.join("build/test-files",
-                                 file.substring(10, file.length - ext.length));
-      const dest = `${destName}_converted.xml`;
+  return Promise.all(files.map(async (file) => {
+    const ext = path.extname(file);
+    const destName = path.join("build/test-files",
+                               file.substring(10, file.length - ext.length));
+    const dest = `${destName}_converted.xml`;
 
-      const tei = await existsInFile(file, /http:\/\/www.tei-c.org\/ns\/1.0/);
-      await mkdirpAsync(path.dirname(dest));
-      if (tei) {
-        await exec(`xsltproc ${xsl} ${file} > ${dest}`);
-      }
-      else {
-        await cprp(file, dest);
-      }
-    })();
-    promises.push(p);
-  }
+    const tei = await existsInFile(file, /http:\/\/www.tei-c.org\/ns\/1.0/);
 
-  Promise.all(promises);
-});
+    await mkdirpAsync(path.dirname(dest));
+    await (tei ? exec(`xsltproc ${xsl} ${file} > ${dest}`) : cprp(file, dest));
+  }));
+}
+
+convert();
